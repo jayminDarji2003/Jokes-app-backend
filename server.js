f27ea3d61db693ad613597a38089c16736197ceb
Jaymin Darji <jaymindarji2003@gmail.com>
2023-11-23 22:06:53 +0530

    adding cors

--- server.js
+++ server.js
@@ -5,9 +5,11 @@
 // module js
 import express from "express"
 import "dotenv/config"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 5000
+app.use(cors());
 
 // app.get("/", (req, res) => {
 //     res.send("<h1>Your server is ready</h1>")
